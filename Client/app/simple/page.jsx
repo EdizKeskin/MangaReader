6e9165b8d522c6aaf979bc04014dcbab4abb232b
Ediz Keskin <edizkeskin@gmail.com>
2025-07-11 00:49:23 +0300

    Bug fix

--- Client/app/simple/page.jsx
+++
@@ -1,5 +0,0 @@
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
-
-export default function Page() {
-  return <SimpleEditor />;
-}
---
+++ Client/components/simple/page.jsx
@@ -0,0 +1,5 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+
+export default function NovelEditor() {
+  return <SimpleEditor />;
+}
