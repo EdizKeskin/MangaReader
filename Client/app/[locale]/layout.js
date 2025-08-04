5e8085a53071a88b13d88cecdb46c7d23f178a08
Ediz Keskin <edizkeskin@gmail.com>
2025-08-04 18:18:41 +0300

    Refactor GTM integration and remove Suspense from layout
    Moved GTM script injection directly into layout.js and simplified GTM.js to only render the noscript iframe. Removed Suspense wrapper around GTM component for more direct rendering.

--- Client/app/[locale]/layout.js
+++ Client/app/[locale]/layout.js
@@ -187,12 +187,20 @@ export default async function RootLayout({ children, params: { locale } }) {
             gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
           `}
           </Script>
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+          `}
+          </Script>
         </head>
       </head>
       <body>
-        <Suspense>
-          <GTM />
-        </Suspense>
+        <GTM />
+
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ClerkProvider
             appearance={{
--- Client/components/GTM.js
+++ Client/components/GTM.js
@@ -1,47 +1,16 @@
 "use client";
 
-import { pageview } from "@/utils/gtm";
-import { usePathname, useSearchParams } from "next/navigation";
-import Script from "next/script";
-import { useEffect } from "react";
-
-export default function Analytics() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (pathname) {
-      pageview(pathname);
-    }
-  }, [pathname, searchParams]);
-
-  if (process.env.NEXT_PUBLIC_NODE_ENV !== "production") {
-    return null;
-  }
-
+export function GTMNoScript() {
   return (
-    <>
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        />
-      </noscript>
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
-  `,
-        }}
-      />
-    </>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
+        `,
+      }}
+    />
   );
 }
